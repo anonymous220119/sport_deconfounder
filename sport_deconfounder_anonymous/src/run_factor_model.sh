b64bb630bcea2d839b5710e97cffa26a008c5dad
anonymous220119 <46927234+anonymous220119@users.noreply.github.com>
2019-01-23 10:12:20 +0100

    Update run_factor_model.sh

--- sport_deconfounder_anonymous/src/run_factor_model.sh
+++ sport_deconfounder_anonymous/src/run_factor_model.sh
@@ -8,10 +8,3 @@ for s in '2016' '2014' '2015' ;do
     done;
 done;
 
-for s in '2019' ;do 
-    for l in 37 49;do 
-	for k in {1..10};do
-	    python factor_model_validation.py $l $k $s;
-	done;
-    done;
-done;
